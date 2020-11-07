a8e1f0244792b00b90b09817a14a3ae83f7a0cca
Shigure19 <1441212902@qq.com>
2020-11-07 23:40:19 +0800

    Add features

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,33 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var util=require('../../utils/util.js')
+var util = require('../../utils/util.js')
 Page({
   data: {
-    login: false,
+    hook: true,
     appointed: false,
-    userData: {
-      userName:NaN,
-      userid: NaN,
-      bathid: NaN,
-      dormid: NaN,
-      starttime: NaN,
-      endtime: NaN,
+    openid: NaN,
+    userWxData: {
+      headicon: NaN,
+      telephone: NaN,
+      nickname: NaN
     },
-    dormData: {},
+    userData: {
+      userName: NaN,
+      userid: NaN,
+      dormid: NaN,
+      appointment: {}
+    },
+    items: [{
+      label: '请输入学号',
+      name: 'userid',
+    }],
+    dormData: {
+      bathroom: NaN,
+      dormid: NaN,
+      now: NaN,
+      total: NaN
+    },
   },
   //事件处理函数
   bindViewTap: function () {
@@ -23,57 +36,96 @@ Page({
     })
   },
   onLoad: function () {
-      //1.设置picker时间为当前时间
-      var mytime=new Date();
-      var time=[mytime.getHours(),mytime.getMinutes()];
-      this.setData({
-        userData:{
-          starttime:time,
-          endtime:time
+    //1.设置picker时间为当前时间
+    var mytime = new Date();
+    var time = [mytime.getHours(), mytime.getMinutes()];
+    this.setData({
+      bootTime: time
+    });
+    //2.获得用户的UnionID并核验
+    wx.login({
+      success: (res) => {
+        if (res.code) {
+          wx.request({
+            url: 'http://127.0.0.1:5000/api/wxlogin/',
+            method: 'POST',
+            data: {
+              'js_code': res.code
+            },
+            method: 'POST',
+            dataType: JSON,
+            header: {
+              'content-type': 'application/json'
+            },
+            success: (res2) => {
+              var data = JSON.parse(res2.data);
+              if (data.status == 'success') {
+                this.setUserStatus(data.userid);
+                this.setDormStatus(data.dormid);
+              } else if (data.status == 'hook') {
+                this.setData({
+                  hook: false,
+                  openid: data.openid
+                })
+              } else if (data.status == 'fail') {
+                this.netError()
+              }
+            },
+          });
+        } else {
+          console.log('登陆失败！' + res.errMsg)
         }
-      })
+      }
+    })
   },
   onPullDownRefresh: function () {
     wx.showToast({
       title: '刷新成功',
       icon: success
     })
-    this.getDormStatus();
-    this.getUserStatus();
+    this.setUserStatus(this.data.userData['userid']);
+    this.setDormStatus(this.data.userData['dormid']);
+
   },
-  login: function (userid, password) {
-    wx.request({
-      url: '/api/login',
-      data: {
-        'userid': userid,
-        'password': password
-      },
-      method: 'POST',
-      dataType: JSON,
-      header: {
-        'content-type': 'application/json'
-      },
-      success(res) {
-        if (res.data['status'] == 'success') {
-          getUserData();
-        } else if (res.data['status'] == 'fail') {
-          console.log('失败！')
-          netError()
+  hook: function (e) {
+    if (e.detail.confirm) {
+      var userData = this.data.userData;
+      userData['userid'] = e.detail.formData['userid']
+      this.setData({
+        userData: userData
+      })
+      wx.request({
+        url: 'http://127.0.0.1:5000/api/hook/',
+        data: {
+          userid: e.detail.formData['userid'],
+          openid: this.data['openid']
+        },
+        dataType: JSON,
+        method: "POST",
+        header: {
+          'content-type': 'application/json'
+        },
+        success: (res) => {
+          var data = JSON.parse(res.data)
+          if (data.status == 'success') {
+            wx.showModal({
+              cancelColor: 'cancelColor',
+              content: '绑定成功'
+            })
+          }
         }
-      },
-      fail() {
-        netError()
-      }
-
-    });
-    if (this.dormData == {}) {
-      this.getDormStatus();
+      })
+    } else {
+      wx.showModal({
+        cancelColor: 'cancelColor',
+        title: '提示',
+        content: '如果不绑定，将无法体验快捷的服务。'
+      })
     }
-
   },
-  getUserStatus: function (userid) {
+  setUserStatus: function (userid) {
     wx.request({
-      url: '/api/getUserStatus',
+      url: 'http://127.0.0.1:5000/api/getUserStatus/',
       data: {
         'userid': userid
       },
@@ -82,50 +134,47 @@ Page({
       header: {
         'content-type': 'application/json'
       },
-      success(res) {
-        if (res.data['status'] == 'success') {
+      success: (res) => {
+        debugger
+        var data = JSON.parse(res.data)
+        if (data.status == 'success') {
           console.log('获取用户信息成功！')
           this.setData({
-            userData: res.data['data']
+            userData: data.userData
           })
-        } else if (res.data['status'] == 'fail') {
+        } else if (data.status == 'fail') {
           console.log('获取用户信息失败！')
         }
-      },
-      fail() {
-        console.log('获取用户信息失败！')
       }
     })
   },
-  getDormStatus: function () {
+  setDormStatus: function (dormid) {
     wx.request({
-      url: '/api/getDormStatus',
+      url: 'http://127.0.0.1:5000/api/getDormStatus/',
       data: {
-        'dormid': userData['dormid']
+        'dormid': dormid
       },
       method: "POST",
       datatype: JSON,
       header: {
         'content-type': 'application/json'
       },
-      success(res) {
-        if (res.data['status'] == 'success') {
+      success: (res) => {
+        var data = JSON.parse(res.data)
+        if (data.status == 'success') {
           console.log('获取浴室信息成功！')
           this.setData({
-            dormData: res.data['data']
+            dormData: data.dormData
           })
-        } else if (res.data['status'] == 'fail') {
+        } else if (data.status == 'fail') {
           console.log('获取浴室信息失败！')
         }
       },
-      fail() {
-        console.log('获取浴室信息失败！')
-      }
     })
   },
   appoint: function (bathid) {
     wx.request({
-      url: '/api/appoint/',
+      url: 'http://127.0.0.1:5000/api/appoint/',
       data: {
         userid: userid,
         bathid: bathid,
@@ -137,8 +186,9 @@ Page({
       header: {
         'content-type': 'application/json'
       },
-      success(res) {
-        if (res.data['status'] == 'success') {
+      success: (res) => {
+        var data = JSON.parse(res.data)
+        if (data.status == 'success') {
           this.setData({
             appointed: true,
             bathid: bathid,
@@ -150,8 +200,8 @@ Page({
             icon: 'success',
           })
           this.onPullDownRefresh()
-        } else if (res.data['status'] == 'fail') {
-          netError()
+        } else if (data.status == 'fail') {
+          this.netError()
         }
       },
       fail() {
@@ -162,19 +212,20 @@ Page({
       },
     })
   },
-  cancel: function (bathid, userid) {
+  cancel: function () {
     wx.request({
-      url: '/api/cancel',
+      url: 'http://127.0.0.1:5000/api/cancel',
       data: {
-        userid: userid,
-        bathid: bathid
+        userid: this.data.userData.userid,
+        bathid: this.data.userData.bathid
       },
       dataType: JSON,
       header: {
         'content-type': 'application/json'
       },
       success(res) {
-        if (res.data['status'] == 'success') {
+        var data = JSON.parse(res.data)
+        if (data.status == 'success') {
           this.setData({
             appointed: false,
             bathid: NaN,
@@ -185,7 +236,7 @@ Page({
             title: '取消成功！',
             icon: 'success',
           })
-        } else if (res.data['status'] == 'fail') {
+        } else if (data.status == 'fail') {
           wx.showToast({
             title: '网络可能存在问题，请重新尝试！',
             icon: 'fail',
@@ -193,81 +244,28 @@ Page({
         }
       },
       fail() {
-        netError()
+        this.netError()
       }
     })
   },
-  starttimeChange:function(e){
+  starttimeChange: function (e) {
     var d = this.data.userData;
-    d.starttime=e.detail.value;
+    d.starttime = e.detail.value;
     this.setData({
-      userData:d
+      userData: d
     })
   },
-  endtimeChange:function(e){
+  endtimeChange: function (e) {
     var d = this.data.userData;
-    d.endtime=e.detail.value;
+    d.endtime = e.detail.value;
     this.setData({
-      userData:d
+      userData: d
     })
-  }, 
-  checkTimeValid:function(){
-    var start=this.data.userData.starttime.slice(0,1)+this.data.userData.starttime.slice(3,4)
-  },
-  showTime:function(){
-    return
   },
   netError: function () {
-    wx.showToast({
+    wx.showModal({
       title: '网络可能存在问题，请重新尝试！',
-      icon: 'fail',
+      cancelColor: 'green',
     })
-  },
-  test: function () {
-    this.setData({
-      login: true,
-      appointed: true,
-      userData: {
-        userName:'张小龙',
-        userid: 19000001,
-        appointment:{
-          status:true,
-          bathid: 236,
-        dormid: 23,
-        starttime: [17,30],
-        endtime: [18,30]
-        }
-        
-      },
-      dormData: {
-        dormId: 23,
-        now: 5,
-        total: 10,
-        bathroom: [{
-          bathid: 231,
-          status:'using'
-        }, {
-          bathid: 232,
-          status:'empty'
-        }, {
-          bathid: 233,
-          status:'using'
-        }, {
-          bathid: 236,
-          status:'appointed'
-        }]
-      }
-    })
-  },
-  appoint_emu: function () {
-    if (this.data.appointed) {
-      this.setData({
-        appointed: false
-      });
-    } else {
-      this.setData({
-        appointed: true
-      });
-    }
   }
 })
